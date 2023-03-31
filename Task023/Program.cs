730ec2c7911c64ec2c6f10aab3770409c6cc10ed
Pavel <pmaskarev@vk.com>
2023-03-31 10:43:10 +0300

    Added task023

---
+++ Task023/Program.cs
@@ -0,0 +1,24 @@
+﻿int Read()
+{
+    System.Console.WriteLine("Введите число:");
+    int x = int.Parse(Console.ReadLine());
+    if (x < 0) throw new ArgumentOutOfRangeException("x");
+    return x;
+}
+
+int x = Read();
+
+void Magic(int x)
+{
+    int a = 1;
+    int b = 0;
+    while (a <= x)
+    {
+        b = (int)Math.Pow(a, 3);
+        a++;
+        System.Console.Write(b + " ");
+    }
+    
+}
+
+Magic(x);
